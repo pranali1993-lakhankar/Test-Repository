6d578110d2b71ba0d08f9bc97db982f8bc6374e8
PRANALI LAKHANKAR <pranalilakhankar@gmail.com>
2023-11-01 16:52:55 +0530

    ABCD

--- tesprojectEclipse/src/javaPackage/Testclass.java
+++ tesprojectEclipse/src/javaPackage/Testclass.java
@@ -5,6 +5,7 @@ public class Testclass {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		System.out.println("Hello Automation");
+		System.out.println("Hey Shammi");
 	}
 
 }
